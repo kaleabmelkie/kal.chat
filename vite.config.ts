dc590e0c24178a1e6d0fb0775e555aba5913ce0b
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2023-03-17 11:48:47 +0300

    Integrate Partytown + GTM

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,13 @@
+import { partytownVite } from '@builder.io/partytown/utils'
 import { sveltekit } from '@sveltejs/kit/vite'
+import path from 'path'
 import type { UserConfig } from 'vite'
 
 export default {
-	plugins: [sveltekit()],
+	plugins: [
+		partytownVite({
+			dest: path.join(__dirname, 'static', '~partytown'),
+		}),
+		sveltekit(),
+	],
 } satisfies UserConfig
