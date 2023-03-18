79edab0f47771cd3a6d27298da1e73dcf1dfb1e1
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2023-03-18 19:45:41 +0300

    Fix gpt3-tokenizer import issue

--- src/lib/utils/count-tokens.ts
+++ src/lib/utils/count-tokens.ts
@@ -1,30 +1,5 @@
-import type GPT3TokenizerImportType from 'gpt3-tokenizer/dist/tokenizer'
-
-let tokenizer: InstanceType<{ new (): GPT3TokenizerImportType }> | undefined = undefined
-
-export async function initTokenizer() {
-	if (tokenizer) {
-		return
-	}
-
-	const GPT3TokenizerImport = await import('gpt3-tokenizer')
-	const GPT3Tokenizer: typeof GPT3TokenizerImport.default =
-		typeof GPT3TokenizerImport.default === 'function'
-			? GPT3TokenizerImport.default
-			: (GPT3TokenizerImport as unknown as typeof GPT3TokenizerImport.default)
-	console.log({ GPT3Tokenizer })
-	tokenizer = new GPT3Tokenizer({
-		type: 'gpt3',
-	})
-}
-
 export async function countTokens(text: string): Promise<number> {
-	if (!tokenizer) {
-		await initTokenizer()
-		return countTokens(text)
-	}
+	const { tokenizer } = await import('./gpt3-tokenizer-import')
 
 	return tokenizer.encode(text).text.length
 }
-
-initTokenizer().catch(console.error)
---
+++ src/lib/utils/gpt3-tokenizer-import.ts
@@ -0,0 +1,8 @@
+import GPT3TokenizerImport from 'gpt3-tokenizer'
+
+const GPT3Tokenizer =
+	typeof GPT3TokenizerImport === 'function'
+		? GPT3TokenizerImport
+		: (GPT3TokenizerImport as { default: typeof GPT3TokenizerImport }).default
+
+export const tokenizer = new GPT3Tokenizer({ type: 'gpt3' })
