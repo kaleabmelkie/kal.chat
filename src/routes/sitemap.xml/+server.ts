4a4529563aabd3bdeef82439ebe24cbb9518571e
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2023-03-26 16:04:18 +0300

    Update sitemap.xml

---
+++ src/routes/sitemap.xml/+server.ts
@@ -0,0 +1,20 @@
+import { version } from '$app/environment'
+
+export async function GET() {
+	return new Response(
+		`
+<?xml version="1.0" encoding="UTF-8" ?>
+<urlset xmlns="https://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>https://kal.chat/account?redirectTo=%2Fthread%2Flatest</loc>
+    <lastmod>${new Date(Number(version)).toISOString()}</lastmod>
+  </url>
+</urlset>
+`.trim(),
+		{
+			headers: {
+				'Content-Type': 'application/xml',
+			},
+		},
+	)
+}
