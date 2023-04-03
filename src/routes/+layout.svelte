4dea27ba1d9cf37840708ca8b167171a6f0049ed
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2023-04-03 19:48:00 +0300

    Fix iOS text selection bug

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,7 +5,12 @@
 </script>
 
 <main class="bg-gradient-to-t from-primary-50 to-primary-100 bg-fixed">
-	<div class="no-repeat bg-fixed" style="background-image: url(/assets/background-patterns/x.svg);">
+	<div
+		class="no-repeat pointer-events-none fixed inset-0 z-0 bg-fixed"
+		style="background-image: url(/assets/background-patterns/x.svg);"
+	/>
+
+	<div class="relative z-10">
 		<Header />
 
 		<slot />
