e9df83979752e5184ed29145954086d83c53e7b2
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2023-04-25 13:41:27 +0300

    Avoid Sentry error captures while in dev mode

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,16 +1,20 @@
+import { dev } from '$app/environment'
 import { PUBLIC_SENTRY_SVELTE_DSN } from '$env/static/public'
 import * as SentrySvelte from '@sentry/svelte'
-import { BrowserTracing } from '@sentry/tracing'
 
-SentrySvelte.init({
-	dsn: PUBLIC_SENTRY_SVELTE_DSN,
-	integrations: [new BrowserTracing()],
-	tracesSampleRate: 1.0,
-})
-SentrySvelte.setTag('svelteKit', 'browser')
+if (!dev) {
+	SentrySvelte.init({
+		dsn: PUBLIC_SENTRY_SVELTE_DSN,
+		integrations: [new SentrySvelte.BrowserTracing()],
+		tracesSampleRate: 1.0,
+	})
+	SentrySvelte.setTag('svelteKit', 'browser')
+}
 
 export const handleError = ({ error, event }) => {
-	SentrySvelte.captureException(error, { contexts: { sveltekit: { event } } })
+	if (!dev) {
+		SentrySvelte.captureException(error, { contexts: { sveltekit: { event } } })
+	}
 
 	return {
 		message: (error as Error)?.message ?? 'Unknown error',
--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,18 +1,24 @@
+import { dev } from '$app/environment'
 import { SENTRY_NODE_DSN } from '$env/static/private'
 import { authHook } from '$lib/hooks/auth-hook.server'
 import * as SentryNode from '@sentry/node'
 import '@sentry/tracing'
 import { sequence } from '@sveltejs/kit/hooks'
 
-SentryNode.init({
-	dsn: SENTRY_NODE_DSN,
-	tracesSampleRate: 1.0,
-	integrations: [new SentryNode.Integrations.Http()],
-})
-SentryNode.setTag('svelteKit', 'server')
+if (!dev) {
+	SentryNode.init({
+		dsn: SENTRY_NODE_DSN,
+		tracesSampleRate: 1.0,
+		integrations: [new SentryNode.Integrations.Http()],
+	})
+	SentryNode.setTag('svelteKit', 'server')
+}
 
 export const handleError = ({ error, event }) => {
-	SentryNode.captureException(error, { contexts: { sveltekit: { event } } })
+	if (!dev) {
+		SentryNode.captureException(error, { contexts: { sveltekit: { event } } })
+	}
+
 	return {
 		message: (error as Error)?.message ?? 'Unknown error',
 	}
