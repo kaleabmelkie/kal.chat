8335b2977c73c737b7890684b647bcfc1a2d78b6
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2023-04-25 12:48:33 +0300

    Integrate Vercel Analytics

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,12 @@
 <script>
 	import '../app.postcss'
 
+	import { dev } from '$app/environment'
 	import { page } from '$app/stores'
 	import Header from '$lib/components/header.svelte'
+	import { inject } from '@vercel/analytics'
+
+	inject({ mode: dev ? 'development' : 'production' })
 </script>
 
 <svelte:head>
