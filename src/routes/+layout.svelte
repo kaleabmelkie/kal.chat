6a4e3100b8a61a7805739c3127da7ce9d5d954c7
Kaleab S. Melkie <kaleabmelkie@gmail.com>
2023-04-25 14:23:26 +0300

    Disable GTM while in dev mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,12 +1,20 @@
 <script>
 	import '../app.postcss'
 
+	import { dev } from '$app/environment'
 	import { page } from '$app/stores'
 	import Header from '$lib/components/header.svelte'
 </script>
 
 <svelte:head>
 	<link rel="canonical" href="{$page.url.origin}{$page.url.pathname}" />
+
+	{#if !dev}
+		<script type="text/partytown">
+			// prettier-ignore
+			(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-T4J9SKX');
+		</script>
+	{/if}
 </svelte:head>
 
 <main
